eee80452666b74f70b26594b526d52aa5d6aeddd
Rajeel Ansari <51192167+rajeeell@users.noreply.github.com>
2023-06-06 00:11:45 +0530

    meal categorizing app

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,56 @@
+import 'package:flutter/material.dart';
+
+import 'package:meals_app/models/category.dart';
+
+const availableCategories = [
+  Categori(
+    id: 'c1',
+    title: 'Italian',
+    color: Colors.purple,
+  ),
+  Categori(
+    id: 'c2',
+    title: 'Quick & Easy',
+    color: Colors.red,
+  ),
+  Categori(
+    id: 'c3',
+    title: 'Hamburgers',
+    color: Colors.orange,
+  ),
+  Categori(
+    id: 'c4',
+    title: 'German',
+    color: Colors.amber,
+  ),
+  Categori(
+    id: 'c5',
+    title: 'Light & Lovely',
+    color: Colors.blue,
+  ),
+  Categori(
+    id: 'c6',
+    title: 'Exotic',
+    color: Colors.green,
+  ),
+  Categori(
+    id: 'c7',
+    title: 'Breakfast',
+    color: Colors.lightBlue,
+  ),
+  Categori(
+    id: 'c8',
+    title: 'Asian',
+    color: Colors.lightGreen,
+  ),
+  Categori(
+    id: 'c9',
+    title: 'French',
+    color: Colors.pink,
+  ),
+  Categori(
+    id: 'c10',
+    title: 'Summer',
+    color: Colors.teal,
+  ),
+];
